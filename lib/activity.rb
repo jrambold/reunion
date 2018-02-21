a46c6f18efcf8b2c00034ec5960539550da6174a
jrambold <jrambold@gmail.com>
2018-02-21 09:43:34 -0700

    activity and particpants added

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,14 @@
 class Activity
+  attr_reader :name,
+              :participants
+
+  def initialize(name)
+    @name = name
+    @participants = {}
+  end
+
+  def add_participant(name, paid_amount)
+    @participants[name] = paid_amount
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,19 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def setup
+    @hiking = Activity.new('hiking')
+  end
+
+  def test_create_activity_with_name
+    assert_equal 'hiking', @hiking.name
+  end
+
+  def test_add_participant
+    @hiking.add_participant('joe', 20)
+    @hiking.add_participant('mary', 30)
+
+    assert_equal 20, @hiking.participants['joe']
+    assert_equal 30, @hiking.participants['mary']
+  end
 end
